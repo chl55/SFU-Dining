ceee44a46aa7d4f169fc1ccdf7af0944bcfe3d73
Michael Duong <michael_duong_2@sfu.ca>
2025-11-05 15:44:26 -0800

    Added detailed restaurant page

--- app/src/main/java/cmpt362/group29/sfudining/restaurants/RestaurantDetail.kt
+++ app/src/main/java/cmpt362/group29/sfudining/restaurants/RestaurantDetail.kt
@@ -1,23 +1,143 @@
 package cmpt362.group29.sfudining.restaurants
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import cmpt362.group29.sfudining.R
 
 @Composable
-fun RestaurantDetail(restaurant: Restaurant?, onBack: () -> Unit) {
-    Column(modifier = Modifier.padding(16.dp)) {
-        Text(text = restaurant?.name ?: "Unknown Restaurant")
-        Spacer(modifier = Modifier.height(100.dp))
-        Button(onClick = onBack) {
-            Text("Back to Map")
+fun RestaurantDetailScreen(restaurant: Restaurant?, onBack: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp)
+    ) {
+        RestaurantImg()
+        Spacer(modifier = Modifier.height(16.dp))
+        RestaurantDesc(restaurant)
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(5.dp),
+            modifier = Modifier.horizontalScroll(rememberScrollState())
+        ) {
+            FeaturedItems()
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(onBack, Modifier.align(Alignment.End)) {
+            Text("Back")
         }
     }
+}
+@Composable
+fun RestaurantImg() {
+    Card(
+        modifier = Modifier.fillMaxWidth().height(275.dp),
+        shape = RoundedCornerShape(10.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.uncle_fatih),
+            contentDescription = "Uncle Fatih's Pizza",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop,
 
+        )
+    }
+}
+
+@Composable
+fun RestaurantDesc(restaurant: Restaurant?) {
+    Text(
+        text = restaurant?.name ?: "Unknown Restaurant",
+        style = MaterialTheme.typography.headlineMedium
+    )
+    Text(
+        text = restaurant?.cuisine ?: "Unknown Cuisine",
+        style = MaterialTheme.typography.bodyMedium
+    )
+    Spacer(modifier = Modifier.height(16.dp))
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Column(modifier = Modifier.weight(1f)) {
+            Text("Phone: ${restaurant?.phoneNum}",
+                style = MaterialTheme.typography.bodySmall)
+            Text("Address: ${restaurant?.address}",
+                style = MaterialTheme.typography.bodySmall)
+
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Column(modifier = Modifier.wrapContentWidth(),
+            horizontalAlignment = Alignment.End) {
+            Text("Schedule:",
+                style = MaterialTheme.typography.bodySmall)
+            restaurant?.schedule?.forEach {
+                (day, hour) ->
+                Text(
+                    "$day - $hour",
+                    style = MaterialTheme.typography.bodySmall
+                )
+
+            }
+        }
+    }
+    Spacer(modifier = Modifier.height(30.dp))
+}
+
+@Composable
+fun FeaturedItems() {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(12.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column {
+            Image(
+                painter = painterResource(id = R.drawable.hot_wings),
+                contentDescription = "Restaurant Image",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp)
+                    .clip(RoundedCornerShape(bottomStart = 12.dp, bottomEnd = 12.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                modifier = Modifier.padding(12.dp)
+            ) {
+                Text(
+                    text = "Mouthwatering Wings",
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.SemiBold,
+                )
+                Text(
+                    text = "8 mouthwatering hot wings",
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.Normal
+                )
+                Text(
+                    text = "$10.95",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    modifier = Modifier.padding(top = 4.dp)
+                )
+            }
+        }
+    }
 }
--- app/src/main/java/cmpt362/group29/sfudining/restaurants/RestaurantNavHost.kt
+++ app/src/main/java/cmpt362/group29/sfudining/restaurants/RestaurantNavHost.kt
@@ -6,18 +6,38 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
+
+data class OpeningHours(
+    val day: String,
+    val hours: String
+)
 data class Restaurant(
     val id: String,
     val name: String,
-    val latLng: LatLng
+    val latLng: LatLng,
+    val cuisine: String,
+    val schedule: List<OpeningHours>,
+    val phoneNum: String,
+    val address: String
 )
 
 @Composable
 fun RestaurantNavHost() {
     val navController = rememberNavController()
     val restaurants = listOf(
-        Restaurant("1", "Uncle Fatih's Pizza", LatLng(49.27811494614226, -122.90997581099475)),
-        Restaurant("2", "Togo Sushi SFU", LatLng(49.27803751562727, -122.90948764895847))
+        Restaurant("1", "Uncle Fatih's Pizza", LatLng(49.27811494614226, -122.90997581099475),
+            "Pizza",
+            listOf(
+                OpeningHours("Monday", "10 a.m.-11 p.m."),
+                OpeningHours("Tuesday", "10 a.m.-11 p.m."),
+                OpeningHours("Wednesday", "10 a.m.-11 p.m."),
+                OpeningHours("Thursday", "10 a.m.-11 p.m."),
+                OpeningHours("Friday", "10 a.m.-11 p.m."),
+                OpeningHours("Saturday", "10 a.m.-11 p.m."),
+                OpeningHours("Sunday", "10 a.m.-9 p.m.")),
+                "(604)-564-6565",
+                "9055 University High St Unit 108, Burnaby, BC V5A 0A7"
+            )
     )
     NavHost(navController, "map") {
         composable("map") {
@@ -32,7 +52,7 @@ fun RestaurantNavHost() {
                 it.id == restId
             }
             restaurant?.let { id ->
-                RestaurantDetail(id) {
+                RestaurantDetailScreen(id) {
                     navController.popBackStack()
                 }
             }
