88bf412e5babfff57006d4a2f5844f7144b4ae97
Michael Duong <michael_duong_2@sfu.ca>
2025-11-04 23:59:52 -0800

    add restaurant map and details

--- app/src/main/java/cmpt362/group29/sfudining/MainActivity.kt
+++ app/src/main/java/cmpt362/group29/sfudining/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import cmpt362.group29.sfudining.restaurants.RestaurantMap
+import cmpt362.group29.sfudining.restaurants.RestaurantNavHost
 import cmpt362.group29.sfudining.ui.theme.SFUDiningTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,6 +27,7 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
+                RestaurantNavHost()
             }
         }
     }
